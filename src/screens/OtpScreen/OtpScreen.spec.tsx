46ce207442e770b7dc3a040063c47edfe6af86f4
pssubramanyabhat <subbukarthikeya98@gmail.com>
2025-04-10 18:29:25 +0530

    Added plop and handlebars to the project

--- src/screens/OtpScreen/OtpScreen.spec.tsx
+++ src/screens/OtpScreen/OtpScreen.spec.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import OtpScreen from './OtpScreen';
 
-
 test('rendered OtpScreen Screen perfectly', () => {
   const tree = renderer.create(<OtpScreen />).toJSON();
   expect(tree).toMatchSnapshot();
