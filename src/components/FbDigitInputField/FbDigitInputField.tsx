ce58edb2819f19e49d506ada248ebb5cf1b1303f
pssubramanyabhat <subbukarthikeya98@gmail.com>
2025-05-13 00:37:22 +0530

    Native Bridging Initiation

--- src/components/FbDigitInputField/FbDigitInputField.tsx
+++ src/components/FbDigitInputField/FbDigitInputField.tsx
@@ -52,7 +52,5 @@ export default FBDigitInputField;
  * Do the same for other components in other screens also.
  * Add interace and types for all components and functions and screens.
  * Need to write test cases for screens also next
- * OTP TEXT Input fields need to be better.
- * The ShortButton -> useRef position moving ahead and backpress scenarios
  * Implement Native Module for sending SMS and then integrate it to RN via Native Bridging
  */
