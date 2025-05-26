5fd2bdb1a45e17ebfde5d6c47d1e1b2556cf6425
pssubramanyabhat <subbukarthikeya98@gmail.com>
2025-05-27 03:27:31 +0530

    Added Test cases for FBSlider Component

--- src/components/FBSlider/FBSlider.spec.tsx
+++ src/components/FBSlider/FBSlider.spec.tsx
@@ -1,18 +1,10 @@
 import React from 'react';
 import {render} from '@testing-library/react-native';
 import FBSlider from './FBSlider';
-import {
-  NativeSyntheticEvent,
-  TextInput,
-  TextInputKeyPressEventData,
-} from 'react-native';
 
-
-describe("render FBSlider correctly", () => {
+describe('render FBSlider correctly', () => {
   it('render the snapshot of the component FBSlider', () => {
-    const snapshot = render(
-      <FBSlider/>,
-    ).toJSON();
+    const snapshot = render(<FBSlider />).toJSON();
     expect(snapshot)?.toMatchSnapshot();
   });
-})
+});
