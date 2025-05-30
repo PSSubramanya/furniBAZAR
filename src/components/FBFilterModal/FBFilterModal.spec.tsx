fea161d53bcbd2b2ef935d25c3205458aa7a3224
pssubramanyabhat <subbukarthikeya98@gmail.com>
2025-05-31 03:35:53 +0530

    Adding test cases to FilterModal Component

--- src/components/FBFilterModal/FBFilterModal.spec.tsx
+++ src/components/FBFilterModal/FBFilterModal.spec.tsx
@@ -1,29 +1,30 @@
 import React from 'react';
 import {render} from '@testing-library/react-native';
 import FBFilterModal from './FBFilterModal';
-import {
-  NativeSyntheticEvent,
-  TextInput,
-  TextInputKeyPressEventData,
-} from 'react-native';
 import imagePath from '../../constants/imagePath';
+import testID from '../../constants/testIdConstants';
+
+jest.mock('react-native-modal', () => {
+  return ({children}: any) => children;
+});
 
 describe('render FBFilterModal correctly', () => {
   const mockFunction = jest.fn();
   const productName = 'Armchair';
-  
-  it('render the snapshot of the component FBFilterModal', () => {
-    const snapshot = render(
+
+  it('mocking FBFilterModal Modal View', () => {
+    const {getByTestId} = render(
       <FBFilterModal
         modalVisible={true}
         setModalVisible={mockFunction}
         selectedProductIcon={imagePath?.armChairIcon}
         selectedProductName={productName}
       />,
-    ).toJSON();
-    expect(snapshot)?.toMatchSnapshot();
+    );
+    const modalViewStatus = getByTestId(testID?.filterModal?.view);
+    expect(modalViewStatus)?.toBeTruthy();
   });
-  
+
   it('render the snapshot of the component FBFilterModal', () => {
     const snapshot = render(
       <FBFilterModal
